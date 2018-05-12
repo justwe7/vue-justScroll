ca354422a72e82ac3fa857815b7cceb4cf802ced
justwe7 <justwe7@users.noreply.github.com>
2018-05-12 11:05:33 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <button style="position: fixed;top:0;right:0;" @click="te">伏击</button>
-    <just-scroll ref="wa" @onBottom="load" :offset="200">
+    <just-scroll ref="wa" @onBottom="load" :offset="100" :auto="true">
       <ul id="xxx">
-        <li v-for="(i,inex) in len">
-          {{inex}}
+        <li v-for="(item,index) in content">
+          ~{{index+1}}···{{item.model}}
         </li>
       </ul>
     </just-scroll>
@@ -19,15 +19,31 @@ export default {
   name: 'app',
   data() {
     return {
-      len: 40
+      n: 1,
+      content: []
     }
   },
   methods: {
-    load() {
-      this.len+=10;
-      setTimeout(() => {
+    fetch() {
+      this.$axios({
+        methods: 'get',
+        url: 'https://easy-mock.com/mock/5ad58a4fbeecc618795e58ca/mkaixin/car/list'
+      }).then((res) => {
+        (this.n===1) && (this.content = [])
+        this.content = [...this.content, ...res.data.data.content]
+        this.n++
         this.$refs.wa.endLoading();
-      }, 2000)
+      }).catch((err)=>{
+        this.$refs.wa.endLoading();
+      })
+    },
+    load() {
+      this.fetch()
+
+//      setTimeout(() => {
+////        this.len+=10;
+//        this.$refs.wa.endLoading();
+//      }, 2000)
     },
     te() {
       this.$refs.wa.destory()
@@ -46,7 +62,7 @@ export default {
   height: 100vh;
   ul{
     li{
-      line-height: 40px;
+      line-height: 80px;
       border-bottom: 1px solid #ccc;
     }
   }
--- src/lib/just-scroll.vue
+++ src/lib/just-scroll.vue
@@ -1,12 +1,14 @@
 <template>
   <section class="just_scroller_wrapper" ref="scroller">
-    <slot ref="main"></slot>
+    <div class="main" :style="transY">
+      <slot ref="main"></slot>
+    </div>
     <!--<ul ref="main" id="xxx">
       <li v-for="(i,inex) in len">
         {{inex+1}}
       </li>
     </ul>-->
-    <div v-show="isLoading" class="loading"><i class="spinner"></i>加载中...</div>
+    <div :class="{show: isLoading}" class="loading"><i class="spinner"></i>加载中...</div>
   </section>
 </template>
 
@@ -17,23 +19,42 @@
         offset: {
           default: 100,
           type: Number
+        },
+        auto: {
+          default: false,
+          type: Boolean
         }
       },
       data() {
           return {
             isLoading: false,//加载的开关
+            moveY: 0,
             wrapperH: 0,//容器高度
             listH: 0,//内容高度
             len: 40
           }
       },
       computed: {
+        transY() {
+          return {
+//            transform: `translate3d(0,${this.moveY}px,0)`
+            transform: `translate3d(0,0px,0)`
+          }
+        },
+        os() {
+          const u = navigator.userAgent;
+          const isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //是否为ios设备
+          const isPC = typeof window.orientation == 'undefined'; //是否为PC端
+          const isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;; //是否为android端
+          return  isIOS && "ios" || isPC && "pc" || isAndroid && "Android"
+        }
 //        listH() {
 //          len
 //        }
       },
       watch: {
         isLoading() {//监听开关 修改内容高度
+//          this.autoFetch()
           this.listH = document.getElementById('xxx').getBoundingClientRect().height;
         }
       },
@@ -43,15 +64,23 @@
         })
       },
       methods: {
+        autoFetch() {//内容高度未满屏 自动加载一组数据
+          if (this.listH < this.wrapperH && this.auto){
+            this._dispathBottom();
+          }
+        },
         scroll(ev) {//滚动事件
-          console.log(367);
+//          console.log(367);
           if (this.isLoading) return false;
           const Top = ev.target.scrollTop;
           if (Top+this.wrapperH+50 > this.listH){
 //            console.log(document.getElementById('xxx').clientHeight);
-            this.loadMore();//关闭提拉加载
-            this.$emit('onBottom')
+            this._dispathBottom();
           }
+        },
+        _dispathBottom() {//派发事件
+          this.loadMore();//关闭提拉加载
+          this.$emit('onBottom');
         },
         startLoading() {//禁止执行事件
           this.isLoading = true
@@ -71,13 +100,32 @@
 //alert(9);
         },
         init() {//初始化绑定事件
+          const Wrap = this.$refs.scroller;//滚动盒子
           this.listH = document.getElementById('xxx').getBoundingClientRect().height;
-          this.wrapperH = this.$refs.scroller.getBoundingClientRect().height;
-//        console.log(this.$refs.scroller.getBoundingClientRect());
+          this.wrapperH = Wrap.getBoundingClientRect().height;
+//        console.log(Wrap.getBoundingClientRect());
 //        console.log(this.$refs.main.clientHeight);
-          this.$refs.scroller.addEventListener("scroll", this.scroll)
+
+          this.autoFetch();
+          Wrap.addEventListener("scroll", this.scroll)
+
+          if (this.os === 'ios' || this.os === 'Android'){
+            Wrap.addEventListener("touchstart", this._sD)
+          }else {
+            Wrap.addEventListener("mousedown", this._sD)
+          }
+          console.log(this.os);
         },
-        destory() {
+        _sD(ev) {
+          const stPointY = ev.changedTouches['0'].clientY;
+//          console.log(ev.changedTouches['0'].clientY);
+          this.$refs.scroller.addEventListener("touchmove", ev=>{
+            const mPointY = ev.changedTouches['0'].clientY;
+            this.moveY = mPointY - stPointY;
+            console.log(mPointY);
+          })
+        },
+        destory() {//卸载事件
           this.$refs.scroller.removeEventListener("scroll", this.scroll)
         }
       },
@@ -91,12 +139,19 @@
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
 
+  /*.main{
+    transform:translate3d(0,10px,0);
+  }*/
 
   .loading{
+    visibility: hidden;
     padding: 10px 0;
     text-align: center;
     font-size: 15px;
     color: #666;
+    &.show{
+      visibility: visible;
+    }
     .spinner{
       display: inline-block;
       margin-right: 10px;
